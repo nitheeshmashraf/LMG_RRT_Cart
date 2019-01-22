71a7658d060af377bf786eaf177168620795b842
nitheeshmashraf <nitheeshmashraf@gmail.com>
2019-01-22 11:20:50 +0400

    Commit with all the live files

---
+++ RR Cart/xav/Untitled-1.js
@@ -0,0 +1,25 @@
+<img src="https://tags.bluekai.com/site/42340?e_id_s42340=7c3bab209194adbe59bd12d836128525f18b6d3399ad1902a0b0a883d95b600d&e_id_m42340=4ecbdcbfa6fb624efbe641ec3d1b33ce" />
+<script type="text/javascript" src="https://tags.bkrtx.com/js/bk-coretag.js"></script>
+<script type="text/javascript">
+        function add_to_cart_request_rrt(dealid){
+            bk_addPageCtx('Aid', '10073');
+            bk_addPageCtx('Etype', 'CART');
+            bk_addPageCtx('Action', 'ADD');
+            bk_addPageCtx('Pid', dealid);
+            bk_addPageCtx('Aggpara01', 'f0d96fba-d1c4-4a3c-999b-8292afe35d74');
+            bk_addPageCtx('Aggpara02', 'dubai~~~~~~~~~~~~~~~~~~~~');
+            bk_addPageCtx('Aggpara03', 'en~~~~~~~~~~~~~~~~~~~~~~~');
+            BKTAG.doTag(42346, 1);
+            BKTAG._reset();
+        }
+
+        function remove_cart_request_rrt(dealid) {
+            bk_addPageCtx('Aid', '10073');
+            bk_addPageCtx('Etype', 'CART');
+            bk_addPageCtx('Action', 'REMOVE');
+            bk_addPageCtx('Pid', dealid);
+            BKTAG.doTag(42346, 1);
+            BKTAG._reset();
+        }
+
+    </script>
